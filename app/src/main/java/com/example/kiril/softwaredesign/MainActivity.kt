dd3b2440991a6e7494622055782b7191afd60b7e
Egor Kirilin <z@xd.cm>
2018-10-26 11:08:13 +0300

    Google template points removed. ShowRequestPermissionsRationalete added. OpenSettings function added too

--- app/src/main/java/com/example/kiril/softwaredesign/MainActivity.kt
+++ app/src/main/java/com/example/kiril/softwaredesign/MainActivity.kt
@@ -2,21 +2,27 @@ package com.example.kiril.softwaredesign
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.telephony.TelephonyManager
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imeiTextView: TextView
     private lateinit var imeiButton: Button
+    private lateinit var rootView: View
 
     private val MY_PERMISSIONS_REQUEST_READ_PHONE_STATE : Int = 1
 
@@ -26,15 +32,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.VersionTextView).apply{
-            text = "${getString(R.string.app_version)}${BuildConfig.VERSION_NAME}"
+            text = "${getString(R.string.app_version)} ${BuildConfig.VERSION_NAME}"
         }
 
         imeiTextView = findViewById(R.id.IMEITextView)
+        rootView = findViewById(R.id.root)
 
         imeiButton = findViewById<Button>(R.id.IMEIButton)
-        imeiButton.setOnClickListener(
-                {requestReadPhoneStatePermissons()}
-        )
+        imeiButton.setOnClickListener{requestReadPhoneStatePermissons()}
 
         requestReadPhoneStatePermissons()
 
@@ -42,32 +47,25 @@ class MainActivity : AppCompatActivity() {
 
 
     fun requestReadPhoneStatePermissons(){
-        // Here, thisActivity is the current activity
+
         if (ContextCompat.checkSelfPermission(this,  Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Permission is not granted
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                             Manifest.permission.READ_PHONE_STATE)) {
-                //Todo fix
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_PHONE_STATE),
-                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                // Display a SnackBar with an explanation and a button to trigger the request.
-                //Todo snackbar after install dependencies
+
+                Snackbar.make(rootView, getString(R.string.ReadPhoneStatePermissonsMessage), Snackbar.LENGTH_INDEFINITE)
+                        .setAction("SUBMIT", View.OnClickListener{
+                            ActivityCompat.requestPermissions(this,
+                                    arrayOf(Manifest.permission.READ_PHONE_STATE),
+                                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+                        })
+                        .show()
             } else {
                 // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(this,
                         arrayOf(Manifest.permission.READ_PHONE_STATE),
                         MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         } else {
             // Permission has already been granted
@@ -83,15 +81,21 @@ class MainActivity : AppCompatActivity() {
             MY_PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
                 // If request is cancelled, the result arrays are empty.
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the work
                     imeiTextView.text = "${getString(R.string.imei_label)} ${getIMEI()}"
-                    imeiButton.visibility = View.INVISIBLE // Todo mb replace by Gone. Invisible takes space. Gone dont take
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
+                    imeiButton.visibility = View.GONE
 
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
+                } else {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                    Manifest.permission.READ_PHONE_STATE)){
+                        Toast.makeText(this, getString(R.string.ReadPhoneStatePermissonsRationalate),
+                                Toast.LENGTH_LONG).show()
+                    }else{
+                        Snackbar.make(rootView, getString(R.string.ReadPhoneStatePermissonsOnDontShowAgain),
+                                Snackbar.LENGTH_LONG)
+                                .setAction("SETTINGS", {OpenSettings()})
+                                .show()
+                    }                }
                 return
             }
 
@@ -101,6 +105,14 @@ class MainActivity : AppCompatActivity() {
                 // Ignore all other requests.
             }
         }
+    }
+
+
+    private fun OpenSettings(){
+        val appSettingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:$packageName"))
+        startActivityForResult(appSettingsIntent, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+        recreate()
     }
 
     private fun getIMEI() : String {
