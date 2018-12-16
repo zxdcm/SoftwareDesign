011c1bc246ae524e2b28295b9f310c935c3169a8
Egor Kirilin <z@xd.cm>
2018-12-16 17:03:19 +0300

    Navigation drawer added. MainActivity migrated to About AboutActivity with a bit of fixes. Profile & Main templates fragments added.

---
+++ app/src/main/java/com/example/kiril/softwaredesign/AboutActivity.kt
@@ -0,0 +1,156 @@
+package com.example.kiril.softwaredesign
+
+import android.content.Intent
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import android.Manifest
+import android.annotation.SuppressLint
+import kotlinx.android.synthetic.main.activity_about.*
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var imeiTextView: TextView
+    private lateinit var imeiButton: Button
+    private lateinit var rootView: View
+    private lateinit var versionTextView: TextView
+
+    private val MY_PERMISSIONS_REQUEST_READ_PHONE_STATE : Int = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        if (getResources().getBoolean(R.bool.portrait_constraint)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        }
+
+        if (savedInstanceState != null){
+            val savedImei = savedInstanceState.getString("IMEI")
+
+            if (savedImei.isNullOrEmpty())        // check does the permissions
+                requestReadPhoneStatePermissons() // were granted after app closing or folding
+            else
+                imeiTextView.text = savedInstanceState.getString("IMEI")
+
+            versionTextView.text = savedInstanceState.getString("VERSION")
+
+            return
+        }
+
+        versionTextView = VersionTextView.apply {
+            text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
+        }
+
+        imeiTextView = IMEITextView
+        rootView = imeiTextView
+
+        imeiButton = IMEIButton
+        imeiButton.setOnClickListener { requestReadPhoneStatePermissons() }
+
+        requestReadPhoneStatePermissons()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState?.putString("IMEI", imeiTextView.text.toString())
+        outState?.putString("VERSION", imeiTextView.text.toString())
+    }
+
+
+
+    private fun requestReadPhoneStatePermissons(){
+
+        if (ContextCompat.checkSelfPermission(this,  Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_PHONE_STATE)) {
+
+                Snackbar.make(rootView, getString(R.string.readPhoneStatePermissonsMessage), Snackbar.LENGTH_INDEFINITE)
+                        .setAction(getString(R.string.submitButton), View.OnClickListener{
+                            ActivityCompat.requestPermissions(this,
+                                    arrayOf(Manifest.permission.READ_PHONE_STATE),
+                                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+                        })
+                        .show()
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_PHONE_STATE),
+                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+            }
+        } else {
+            setIMEIView()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the work
+                    setIMEIView()
+                } else {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                                    Manifest.permission.READ_PHONE_STATE)){
+                        Toast.makeText(this, getString(R.string.readPhoneStatePermissonsRationalate),
+                                Toast.LENGTH_LONG).show()
+                    }else{
+                        Snackbar.make(rootView, getString(R.string.readPhoneStatePermissonsOnDontShowAgain),
+                                Snackbar.LENGTH_LONG)
+                                .setAction(getString(R.string.settingsButton), {OpenSettings()})
+                                .show()
+                    }                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
+
+
+    private fun OpenSettings(){
+        val appSettingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:$packageName"))
+        startActivityForResult(appSettingsIntent, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
+    }
+
+    private fun setIMEIView(){
+        imeiTextView.text = getString(R.string.imei_label, getIMEI())
+        imeiButton.visibility = View.GONE
+
+    }
+
+    @SuppressLint("HardwareIds")
+    private fun getIMEI() : String {
+        try{
+            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val IMEI = tm.deviceId
+            return IMEI
+        }
+        catch (e: SecurityException){
+            throw e
+        }
+    }
+}
--- app/src/main/java/com/example/kiril/softwaredesign/MainActivity.kt
+++ app/src/main/java/com/example/kiril/softwaredesign/MainActivity.kt
@@ -1,156 +1,87 @@
 package com.example.kiril.softwaredesign
 
-import android.Manifest
-import android.content.Context
+
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.provider.Settings
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import android.telephony.TelephonyManager
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 
-class MainActivity : AppCompatActivity() {
 
-    // todo maybe replace later
-    private lateinit var imeiTextView: TextView
-    private lateinit var imeiButton: Button
-    private lateinit var rootView: View
-    private lateinit var versionTextView: TextView
+class MainActivity : AppCompatActivity(),
+            NavigationView.OnNavigationItemSelectedListener,
+            MainFragment.OnFragmentInteractionListener,
+            ProfileFragment.OnFragmentInteractionListener {
 
-    private val MY_PERMISSIONS_REQUEST_READ_PHONE_STATE : Int = 1
+        override fun onFragmentInteraction(uri: Uri) {}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        private lateinit var navController: NavController
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        if (getResources().getBoolean(R.bool.portrait_constraint)) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+            setSupportActionBar(toolbar)
+            navController = findNavController(R.id.nav_host_fragment)
+
+            val toggle = ActionBarDrawerToggle(
+                    this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+            drawer_layout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            nav_view.setNavigationItemSelectedListener(this)
+            nav_view.setCheckedItem(R.id.nav_home)
+
         }
 
-        if (savedInstanceState != null){
-            val savedImei = savedInstanceState.getString("IMEI")
-
-            if (savedImei.isNullOrEmpty())        // check does the permissions
-                requestReadPhoneStatePermissons() // were granted after app closing or folding
-            else
-                imeiTextView.text = savedInstanceState.getString("IMEI")
-
-            versionTextView.text = savedInstanceState.getString("VERSION")
-
-            return
-        }
-
-        versionTextView = VersionTextView.apply {
-            text = getString(R.string.app_version, BuildConfig.VERSION_NAME)
-        }
-
-        imeiTextView = IMEITextView
-        rootView = root
-
-        imeiButton = IMEIButton
-        imeiButton.setOnClickListener { requestReadPhoneStatePermissons() }
-
-        requestReadPhoneStatePermissons()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?, outPersistentState: PersistableBundle?) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        outState?.putString("IMEI", imeiTextView.text.toString())
-        outState?.putString("VERSION", imeiTextView.text.toString())
-    }
-
-
-
-    fun requestReadPhoneStatePermissons(){
-
-        if (ContextCompat.checkSelfPermission(this,  Manifest.permission.READ_PHONE_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.READ_PHONE_STATE)) {
-
-                Snackbar.make(rootView, getString(R.string.readPhoneStatePermissonsMessage), Snackbar.LENGTH_INDEFINITE)
-                        .setAction(getString(R.string.submitButton), View.OnClickListener{
-                            ActivityCompat.requestPermissions(this,
-                                    arrayOf(Manifest.permission.READ_PHONE_STATE),
-                                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-                        })
-                        .show()
+        override fun onBackPressed() {
+            if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+                drawer_layout.closeDrawer(GravityCompat.START)
             } else {
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.READ_PHONE_STATE),
-                        MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-            }
-        } else {
-          setIMEIView()
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission was granted, yay! Do the work
-                    setIMEIView()
-                } else {
-                    if(ActivityCompat.shouldShowRequestPermissionRationale(this,
-                                    Manifest.permission.READ_PHONE_STATE)){
-                        Toast.makeText(this, getString(R.string.readPhoneStatePermissonsRationalate),
-                                Toast.LENGTH_LONG).show()
-                    }else{
-                        Snackbar.make(rootView, getString(R.string.readPhoneStatePermissonsOnDontShowAgain),
-                                Snackbar.LENGTH_LONG)
-                                .setAction(getString(R.string.settingsButton), {OpenSettings()})
-                                .show()
-                    }                }
-                return
-            }
-
-            // Add other 'when' lines to check for other
-            // permissions app might request.
-            else -> {
-                // Ignore all other requests.
+                super.onBackPressed()
             }
         }
-    }
 
-
-    private fun OpenSettings(){
-        val appSettingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                Uri.parse("package:$packageName"))
-        startActivityForResult(appSettingsIntent, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE)
-    }
-
-    private fun setIMEIView(){
-        imeiTextView.text = getString(R.string.imei_label, getIMEI())
-        imeiButton.visibility = View.GONE
-
-    }
-
-    private fun getIMEI() : String {
-        try{
-            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-            val IMEI = tm.getDeviceId()
-            return IMEI
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            menuInflater.inflate(R.menu.main, menu)
+            return true
         }
-        catch (e: SecurityException){
-            throw e
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            when (item.itemId) {
+                R.id.action_about -> {
+                    startActivity(Intent(this, AboutActivity::class.java))
+                    return true
+                }
+                else -> return super.onOptionsItemSelected(item)
+            }
         }
-    }
+
+        override fun onNavigationItemSelected(item: MenuItem): Boolean {
+            // Handle navigation view item clicks here.
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    navController.navigate(R.id.mainFragment)
+                }
+                R.id.nav_profile -> {
+                    navController.navigate(R.id.profileFragment)
+                }
+            }
+
+            drawer_layout.closeDrawer(GravityCompat.START)
+            return true
+        }
 }
